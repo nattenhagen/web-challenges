9dcb2e84615310caac3ce96c15bf6be7889f5724
Natascha Tenhagen <nattenhagen@googlemail.com>
2024-03-21 14:52:32 +0100

    add all challanges

--- js-error-handling_fetch/js/index.js
+++ js-error-handling_fetch/js/index.js
@@ -1,34 +1,86 @@
+// console.clear();
+
+// const userElement = document.querySelector(".user");
+// const errorElement = document.querySelector(".error");
+
+// async function getUser(url) {
+//   const response = await fetch(url);
+//   console.log(response);
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch user data");
+//   }
+//   try {
+//     const json = await response.json();
+//     errorElement.textContent = "";
+//     return json.data;
+//   } catch (error) {
+//     errorElement.textContent = "Error: " + error.message;
+//   }
+// }
+
+// document.querySelectorAll("button[data-url]").forEach((button) =>
+//   button.addEventListener("click", async (event) => {
+//     try {
+//       errorElement.textContent = "";
+//       const user = await getUser(event.target.dataset.url);
+//       userElement.innerHTML = `
+//       <h2>${user.first_name} ${user.last_name}</h2>
+//       <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+//       `;
+//     } catch {
+//       userElement.innerHTML = "User doesn't exisit!.";
+//     }
+//   })
+// );
+
 console.clear();
 
+/**
+ * This is NOT the ONLY solution that works for this challenge
+ */
 const userElement = document.querySelector(".user");
 const errorElement = document.querySelector(".error");
 
+// console.info() is not part of the solution
+// .info() works the same as console log
+// I am using it here as an example of how I would inspect variables to get to the solution
+// console.info() is here only for descriptive purposes
 async function getUser(url) {
+  userElement.innerHTML = ""; // clear previous user information
+  errorElement.textContent = ""; // clear previous error
+
   const response = await fetch(url);
-  console.log(response);
+  console.info("response variable:", response); // we inspect response, focus on parameters "status" and "ok"
   if (!response.ok) {
-    throw new Error("Failed to fetch user data");
+    // we check if response is NOT ok, status >= 400 is not ok
+    console.log("Network error");
+    return null; // there is nothing to return
+    // but our code expects to receive something so we return null
   }
-  try {
-    const json = await response.json();
-    errorElement.textContent = "";
-    return json.data;
-  } catch (error) {
-    errorElement.textContent = "Error: " + error.message;
-  }
+
+  const json = await response.json(); // if response data is not in JSON format, this will throw an error
+  return json.data;
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     try {
-      errorElement.textContent = "";
       const user = await getUser(event.target.dataset.url);
+      console.info("user variable:", user); // we inspect the user variable
+      if (!user) {
+        // if user is null, then !null is the same as true, the condition for if is met
+        errorElement.textContent = "User not found."; // no user data, so no user found
+        return; // no user information to display, we end function here
+      }
+
       userElement.innerHTML = `
       <h2>${user.first_name} ${user.last_name}</h2>
       <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
       `;
-    } catch {
-      userElement.innerHTML = "User doesn't exisit!.";
+    } catch (error) {
+      console.info(error); // we inspect the error variable
+      errorElement.textContent = error.message; // all error objects have message property
+      return; // invalid format, nothing to display, we end function here
     }
   })
 );
