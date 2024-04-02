db38a36870c31e6dbb95b7700288b56d0cde82b1
Natascha Tenhagen <nattenhagen@googlemail.com>
2024-04-02 11:39:53 +0200

    add challenge

---
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
+        );
+        const data = await response.json();
+        setPokemon(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadPokemon();
+  }, [offset]); // Trigger useEffect when offset changes
+
+  const handleNextPage = () => {
+    setOffset(offset + 20);
+  };
+
+  const handlePreviousPage = () => {
+    if (offset >= 20) {
+      setOffset(offset - 20);
+    }
+  };
+
+  return (
+    <main>
+      <button type="button" onClick={handlePreviousPage}>
+        Previous Page
+      </button>
+      <button type="button" onClick={handleNextPage}>
+        Next Page
+      </button>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
